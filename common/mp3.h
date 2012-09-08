f506842df2b0e251b56ffe0a828bb2029d423eec
notaz <notasas@gmail.com>
2012-09-08 20:31:36 +0300

    remove PicoDrive-specific stuff, add readme

--- common/mp3.h
+++
@@ -1,8 +0,0 @@
-
-int mp3_find_sync_word(const unsigned char *buf, int size);
-
-#ifdef __GP2X__
-void mp3_update_local(int *buffer, int length, int stereo);
-void mp3_start_play_local(void *f, int pos);
-#endif
-
