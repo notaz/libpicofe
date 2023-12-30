fa4a662a346cda35ba018e38f5bff6e0a36b9102
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2023-12-30 21:53:41 +0100

    Using PSP as posix

--- posix.h
+++ posix.h
@@ -2,7 +2,7 @@
 #define LIBPICOFE_POSIX_H
 
 /* define POSIX stuff: dirent, scandir, getcwd, mkdir */
-#if defined(__MACH__) || defined(__linux__) || defined(__MINGW32__)
+#if defined(__MACH__) || defined(__linux__) || defined(__MINGW32__) || defined(__PSP__) || defined(__PS2__)
 
 #include <dirent.h>
 #include <unistd.h>
@@ -15,21 +15,6 @@
 #define d_type d_ino
 #define DT_REG 0
 #define DT_DIR 0
-#endif
-
-#elif defined(__PSP__)
-
-#include <dirent.h>
-#include <unistd.h>
-#include <sys/stat.h>
-
-#ifndef DT_DIR
-/* map PSP names to posix. needs special scandir() function to mask rwx bits */
-#define d_type d_stat.st_attr
-#define DT_LNK FIO_SO_IFLNK
-#define DT_DIR FIO_SO_IFDIR
-#define DT_REG FIO_SO_IFREG
-#define DT_UNKNOWN 0
 #endif
 
 #else
